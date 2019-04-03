dd20d4d5330fdb3f3d96ac034e3b27e029d8829c
XingZengZhi <944047118@qq.com>
2019-04-03 17:58:51 +0800

    commit

--- control.go
+++ control.go
@@ -1,13 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 func main() {
 	i, count := 0, 0
 
-	for ;i <= 100;i++ {
+	for ; i <= 100; i++ {
 		count += i
 	}
 
 	fmt.Println(count)
+
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		fmt.Printf("%s", os)
+	}
 }
